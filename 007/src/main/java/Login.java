725e8068cc18bf86a305f3f676277eb901f8d6b6
adubey <adubey@INDHYDELP211.planit.local>
2022-03-22 16:24:45 +0530

    Try kar

--- 007/src/main/java/Login.java
+++ 007/src/main/java/Login.java
@@ -4,5 +4,8 @@ public class Login
 public void print()
 {
 	System.out.println("Wake up to reality");
+	System.out.println("ZA WARUDO TEKKI WA TOMOREI");
+	System.out.println("THE NAME'S BOND, JAMES BOND.");
+	System.out.println("don't care!");
 }
 }
